ed7c10417cb33d6b4feb0f82589c1047fec03e49
Fernando R. Conde <fernando.conde@totemti.com.br>
2022-08-26 14:54:19 -0400

    fix: correct imports module

--- dist/app.module.js
+++ dist/app.module.js
@@ -13,13 +13,12 @@ const app_service_1 = require("./app.service");
 const prisma_module_1 = require("./prisma/prisma.module");
 const user_module_1 = require("./user/user.module");
 const auth_module_1 = require("./auth/auth.module");
-const autj_controller_1 = require("./autj/autj.controller");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     (0, common_1.Module)({
         imports: [prisma_module_1.PrismaModule, user_module_1.UserModule, auth_module_1.AuthModule],
-        controllers: [app_controller_1.AppController, autj_controller_1.AutjController],
+        controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
 ], AppModule);
